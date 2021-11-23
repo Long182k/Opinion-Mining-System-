0e2e3ef9416fb82a2bb2465e67a1bd7cd158354d
Long182k <thanhlong9123@gmail.com>
2021-11-23 16:23:21 +0700

    re

--- init.py
+++ init.py
@@ -4,6 +4,15 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app)
+api = Api(app)
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 
