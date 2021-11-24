44c3d9db1836f038abc19300775a5c90e2f0226d
Long182k <thanhlong9123@gmail.com>
2021-11-24 16:07:20 +0700

    zup

--- init.py
+++ init.py
@@ -3,20 +3,20 @@ from flask_cors import CORS
 from flask_restful import Api
 
 app = Flask(__name__)
-# cors = CORS(app)
+cors = CORS(app)
 api = Api(app)
 
-CORS(app, origins="http://localhost:5500", allow_headers=[
-    "Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
-    supports_credentials=True)
+# CORS(app, origins="http://localhost:5500", allow_headers=[
+#     "Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
+#     supports_credentials=True)
 
 
-@app.after_request
-def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DEL ETE,OPTIONS')
-  return response
+# @app.after_request
+# def after_request(response):
+#   response.headers.add('Access-Control-Allow-Origin', '*')
+#   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+#   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DEL ETE,OPTIONS')
+#   return response
 
 
 app.config['CORS_HEADERS'] = 'Content-Type'
